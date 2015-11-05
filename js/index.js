60decee7b2ebd0eb8d36639f46921a8222a3327a
cangMin <598069696@qq.com>
2015-11-05 19:01:37 +0800

    自定义下拉框按钮

---
+++ js/index.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	
+	$(".icon-select").mouseover(function(){
+		$("#options").css("display","block");
+	});
+	$("#options").mouseover(function(){
+		$("#options").css("display","block");
+	});
+	$(".icon-select").mouseout(function(){
+		$("#options").css("display","none");
+	});
+	$("#options").mouseout(function(){
+		$("#options").css("display","none");
+	});
+	});
